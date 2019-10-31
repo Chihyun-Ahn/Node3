f5cd500c8a7ccfb2e94292b11e239c60064deeb8
Chihyun-Ahn <anchi15@naver.com>
2019-10-31 13:40:41 +0900

    can통신 제대로 안됨.작업중..

--- dataBean.js
+++ dataBean.js
@@ -2,39 +2,15 @@ const TARTEMPDEFAULT = 23.0;
 const TEMPBANDDEFAULT = 6.0;
 
 var house1 = {
-    num: 0, 
-    curTime: "-", 
-    msgTime: "-", 
-    tarTemp: TARTEMPDEFAULT,
-    tempBand: TEMPBANDDEFAULT, 
-    ventilPer: -99,
-    temp1: 0, temp2: 0, 
-    avgTemp: 0, 
-    humid1: 0, humid2: 0, 
-    avgHumid: 0, 
-    fanMode: 0, 
-    fan1: 0, fan2: 0, fan3: 0,
-    waterMode: 0, 
-    water: 0, 
-    alarm: 0
+    num: 0, curTime: "-", msgTime: ["-","-","-"], tarTemp: TARTEMPDEFAULT,tempBand: TEMPBANDDEFAULT, ventilPer: -99,
+    temp: [0,0,0,0,0,0], avgTemp: 0, humid: [0,0,0,0,0,0], avgHumid: 0, fanMode: 0, fan: [0,0,0],
+    waterMode: 0, water: 0, alarm: 0
 };
 
 var house2 = {
-    num: 0, 
-    curTime: "-", 
-    msgTime: "-", 
-    tarTemp: TARTEMPDEFAULT, 
-    tempBand: TEMPBANDDEFAULT, 
-    ventilPer: -99,
-    temp1: 0, temp2: 0, 
-    avgTemp: 0, 
-    humid1: 0, humid2: 0, 
-    avgHumid: 0, 
-    fanMode: 0, 
-    fan1: 0, fan2: 0, fan3: 0,
-    waterMode: 0, 
-    water: 0, 
-    alarm: 0
+    num: 0, curTime: "-", msgTime: ["-","-","-"], tarTemp: TARTEMPDEFAULT, tempBand: TEMPBANDDEFAULT, ventilPer: -99,
+    temp: [0,0,0,0,0,0], avgTemp: 0, humid: [0,0,0,0,0,0], avgHumid: 0, fanMode: 0, fan: [0,0,0],
+    waterMode: 0, water: 0, alarm: 0
 };
 
 var house = [house1,house2];
