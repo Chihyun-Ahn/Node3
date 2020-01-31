198209957ec5d240c50746ca24c624d0be63d83b
Chihyun-Ahn <anchi15@naver.com>
2020-01-31 17:49:53 +0900

    20200131 -2

--- dataBean.js
+++ dataBean.js
@@ -2,10 +2,10 @@ const TARTEMPDEFAULT = 23.0;
 const TEMPBANDDEFAULT = 6.0;
 
 var house1 = {
-    num: 0, msgID: '-', edgeDepTime: '-', fogArrTime: '-', fogDepTime: '-', userArrTime: '-',
-    tarTemp: TARTEMPDEFAULT,tempBand: TEMPBANDDEFAULT, ventilPer: -99,temp: [0,0,0,0,0,0], 
-    avgTemp: 0.0, humid: [0,0,0,0,0,0], avgHumid: 0.0, fanMode: 0, fan: [0,0,0],
-    waterMode: 0, water: 0, alarm: 0, state: 0 
+    num: 0, msgID: '-', edgeDepTime: '-', fogArrTime: '-', fogDepTime: '-', cloudArrTime: '-', 
+    userArrTime: '-', tarTemp: TARTEMPDEFAULT,tempBand: TEMPBANDDEFAULT, ventilPer: -99,
+    temp: [0,0,0,0,0,0], avgTemp: 0.0, humid: [0,0,0,0,0,0], avgHumid: 0.0, fanMode: 0, 
+    fan: [0,0,0], waterMode: 0, water: 0, alarm: 0, state: 0 
 };
 
 /*
@@ -16,10 +16,10 @@ var house1 = {
 */
 
 var house2 = {
-    num: 0, msgID: '-', edgeDepTime: '-', fogArrTime: '-', fogDepTime: '-', userArrTime: '-',
-    tarTemp: TARTEMPDEFAULT,tempBand: TEMPBANDDEFAULT, ventilPer: -99,temp: [0,0,0,0,0,0], 
-    avgTemp: 0.0, humid: [0,0,0,0,0,0], avgHumid: 0.0, fanMode: 0, fan: [0,0,0],
-    waterMode: 0, water: 0, alarm: 0, state: 0 
+    num: 0, msgID: '-', edgeDepTime: '-', fogArrTime: '-', fogDepTime: '-', cloudArrTime: '-', 
+    userArrTime: '-', tarTemp: TARTEMPDEFAULT,tempBand: TEMPBANDDEFAULT, ventilPer: -99,
+    temp: [0,0,0,0,0,0], avgTemp: 0.0, humid: [0,0,0,0,0,0], avgHumid: 0.0, fanMode: 0, 
+    fan: [0,0,0], waterMode: 0, water: 0, alarm: 0, state: 0 
 };
 
 var house = [house1,house2];
